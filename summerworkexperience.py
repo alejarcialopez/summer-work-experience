ae18627374deb6d86b85a2d0f9ebcc155e17c299
Andres <alejarcialopez@gmail.com>
2017-08-04 15:46:15 +0100

    file as python module

--- summerworkexperience.py
+++ summerworkexperience.py
@@ -2,34 +2,24 @@ import csv
 from datetime import *
 import calendar
 import requests
-import secrets
 import argparse
 
-f = open('dropbox_licenses2.csv', 'r')
 
-linelist = f.readlines()
-lastline = linelist[len(linelist) - 1].split(",")
-
-parser = argparse.ArgumentParser(description="parse database of licenses and post rolling total and timestamps")
-parser.add_argument("dbfile", type=str, help="file to be parsed")
-parser.add_argument("timeseries", type=str, help="name of time series")
-parser.add_argument("step", type=int, help="interval of how quickly measurements are taken")
-parser.add_argument("-sd", "--stopdate", dest="stopd", type=str, action="store",
-                    default=lastline[0], help="store stop date (default: last date of file), date format: YYYY-MM-DD")
-parser.add_argument("url", type=str, help="server url")
-parser.add_argument("database", type=str, help="database to write to")
-parser.add_argument("user", type=str, help="username")
-parser.add_argument("password", type=str, help="password")
-
-args = parser.parse_args()
+def getlastline(dbfile):
+    f = open(dbfile, 'r')
+    linelist = f.readlines()
+    lastline = linelist[len(linelist) - 1].split(",")
+    f.close()
+    return lastline
 
 
-def parsing(dbfile=args.dbfile):
+def parsing(dbfile, stopd):
+    lastline = getlastline(dbfile)
     count = 1
     lTimestamps = []
     ldates = []
     try:
-        stop = datetime.strptime(args.stopd, "%Y-%m-%d")
+        stop = datetime.strptime(stopd, "%Y-%m-%d")
     except TypeError or ValueError:
         print("follow format YYYY-MM-DD")
         exit()
@@ -50,34 +40,31 @@ def parsing(dbfile=args.dbfile):
     print("it worked once")
     return lTimestamps
 
-lValues = parsing()
 
-
-def postvalues(timeseries=args.timeseries, step=args.step, serverurl=args.url,
-               INFLUX_DB=args.database, INFLUX_USER=args.user, INFLUX_PASS=args.password):
+def postvalues(timeseries, step, serverurl,
+               influx_db, influx_user, influx_pas, lvalues):
     url = serverurl
-    payload = {"db": INFLUX_DB, "u": INFLUX_USER, "p": INFLUX_PASS}
+    payload = {"db": influx_db, "u": influx_user, "p": influx_pas}
     rollingtotal = 0
     k = 0
-    for k in range(0, len(lValues) - 1):
+    for k in range(0, len(lvalues) - 1):
         postvalues = ""
-        rollingtotal += lValues[k]['v']
-        for x in range(lValues[k]['ts'], lValues[k + 1]['ts'], step):
+        rollingtotal += lvalues[k]['v']
+        for x in range(lvalues[k]['ts'], lvalues[k + 1]['ts'], step):
             postvalues += f'{timeseries},type_instance=num_provisioned_users,' \
                           f'ds_index=0,ds_name=value,' \
                           f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
                           f'plugin=statsd,state=ok,type=gauge ' \
                           f'value={rollingtotal} {x}\n'
         # r = requests.post(url, params=payload, data=postvalues)
-        rollingtotal += lValues[k]['v']
+        rollingtotal += lvalues[k]['v']
 
     postvalues = f'{timeseries},type_instance=num_provisioned_users,' \
                  f'ds_index=0,ds_name=value,' \
                  f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
                  f'plugin=statsd,state=ok,type=gauge ' \
-                 f'value={lValues[k + 1]["v"] + rollingtotal} {x + step}'
+                 f'value={lvalues[k + 1]["v"] + rollingtotal} {x + step}'
     # check2
     print("it worked twice")
     # r = requests.post(url, params=payload, data=postvalues)
 
-postvalues()
