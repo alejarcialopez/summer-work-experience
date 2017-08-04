41840b3425759840e538fa443eabeba52e5c497a
Andres <alejarcialopez@gmail.com>
2017-08-04 10:32:16 +0100

    add command line interface

--- summerworkexperience.py
+++ summerworkexperience.py
@@ -3,11 +3,16 @@ from datetime import *
 import calendar
 import requests
 import secrets
+import argparse
 
-# f = open('tsandv.txt', 'w')
+parser = argparse.ArgumentParser(description="parse database of licenses and post rolling total and timestamps")
+parser.add_argument("dbfile", type=str, help="file to be parsed")
+parser.add_argument("timeseries", type=str, help="name of time series")
+parser.add_argument("step", type=int, help="interval of how quickly measurements are taken")
+args = parser.parse_args()
 
 
-def parsing(dbfile, timeseries, step):
+def parsing(dbfile=args.dbfile):
     count = 1
     lTimestamps = []
     ldates = []
@@ -25,37 +30,35 @@ def parsing(dbfile, timeseries, step):
                 lTimestamps.append({"ts": ldates[location],
                                     "v": count})
                 count = 1
+    # check2 print("it worked once")
+    return lTimestamps
 
+lValues = parsing()
+
+
+def postvalues(timeseries=args.timeseries, step=args.step):
     url = 'http://ss-metrics.srv.uis.private.cam.ac.uk:8086/write'
     payload = {"db": "aa2012", "u": secrets.INFLUX_USER, "p": secrets.INFLUX_PASS}
     rollingtotal = 0
     k = 0
-    for k in range(0, len(lTimestamps) - 1):
+    for k in range(0, len(lValues) - 1):
         postvalues = ""
-        rollingtotal += lTimestamps[k]['v']
-        for x in range(lTimestamps[k]['ts'], lTimestamps[k + 1]['ts'], step):
+        rollingtotal += lValues[k]['v']
+        for x in range(lValues[k]['ts'], lValues[k + 1]['ts'], step):
             postvalues += f'{timeseries},type_instance=num_provisioned_users,' \
                           f'ds_index=0,ds_name=value,' \
                           f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
                           f'plugin=statsd,state=ok,type=gauge ' \
                           f'value={rollingtotal} {x}\n'
-        # r = requests.post(url, params=payload, data=postvalues)
-        rollingtotal += lTimestamps[k]['v']
+        r = requests.post(url, params=payload, data=postvalues)
+        rollingtotal += lValues[k]['v']
 
     postvalues = f'{timeseries},type_instance=num_provisioned_users,' \
                  f'ds_index=0,ds_name=value,' \
                  f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
                  f'plugin=statsd,state=ok,type=gauge ' \
-                 f'value={lTimestamps[k + 1]["v"] + rollingtotal} {x + step}'
+                 f'value={lValues[k + 1]["v"] + rollingtotal} {x + step}'
+    # check1 print("it worked twice")
+    r = requests.post(url, params=payload, data=postvalues)
 
-    # r = requests.post(url, params=payload, data=postvalues)
-
-    # print(r.status_code)
-    # print(r.reason)
-# for element in range(0, len(newdictionary) - 1):
-#    f.write(str(newdictionary[element]))
-#    f.write("\n")
-
-# f.close()
-
-parsing('dropbox_licenses2.csv', 'statsd/gauge-num_provisioned_users', 2000000000)
+postvalues()
