6daa13f9a7e73a9fea29aa2ce2f5cd38265e9d94
Andres <alejarcialopez@gmail.com>
2017-08-07 16:46:29 +0100

    post values as two lines are read

--- backfill.py
+++ backfill.py
@@ -9,84 +9,46 @@ def parse_post(dbfile, options):
     location = 1
     url = options.url
     payload = {"db": options.influx_db, "u": options.influx_user, "p": options.influx_pass}
-    rollingtotal = 0
     count = 1
-    lTimestamps = []
-    ldates = []
-    pairdate1 = None
-    pairdate2 = None
     postvalues = ""
-    with open(dbfile, 'r') as DBtoparse:
-        content = list(csv.DictReader(DBtoparse))
-        if args.stopd:
-            try:
-                stopdate = datetime.strptime(args.stopd, "%Y-%m-%d")
-            except TypeError or ValueError:
-                print("follow date format: YYYY-MM-DD")
-                exit()
-            for line in content:
-                try:
-                    nextline = content[location]
-                except IndexError:
-                    nextline = content[location - 1]
-                if line['active'] == 'True':
-                    if datetime.strptime(line['start_date'], "%Y-%m-%d") <= stopdate and \
-                                    line['start_date'] == nextline['start_date']:
-
-                        pairdate1 = int(calendar.timegm(datetime.strptime(line['start_date'], "%Y-%m-%d")).timetuple() *
-                                        1000000000)
-                        count += 1
-                    elif datetime.strptime(line['start_date'], "%Y-%m-%d") <= stopdate and \
-                            line['start_date'] != nextline['start_date']:
-
-                        pairdate2 = int(
-                            calendar.timegm(datetime.strptime(nextline['start_date'], "%Y-%m-%d")).timetuple() *
+    with open(dbfile, 'r') as content:
+        headers = content.readline()
+        line1 = content.readline()
+        for line in content:
+            list1 = []
+            list2 = []
+            line2 = content.readline()
+            list1.append(line1.split(','))
+            list2.append(line2.split(','))
+            date1 = datetime.strptime(list1[0][0], "%Y-%m-%d")
+            pairdate1 = int(calendar.timegm(date1.timetuple()) *
                             1000000000)
-                        rollingtotal += count
-                        if pairdate1 is None:
-                            date1 = datetime.strptime(line['start_date'], "%Y-%m-%d")
-                            pairdate1 = int(calendar.timegm(date1.timetuple()) *
-                                            1000000000)
+            date2 = datetime.strptime(list2[0][0], "%Y-%m-%d")
+            pairdate2 = int(calendar.timegm(date2.timetuple()) *
+                            1000000000)
+            x = 0
+            if list2[0][0] != '':
+                if list1[0][2] == 'True\n' and list2[0][2] == 'True\n':
+                    if list1[0][0] == list2[0][0]:
+                        count += 1
+                    elif list1[0][0] != list2[0][0]:
                         for x in range(pairdate1, pairdate2, options.step):
                             postvalues += f'{options.timeseries},type_instance=num_provisioned_users,' \
                                           f'ds_index=0,ds_name=value,' \
                                           f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
                                           f'plugin=statsd,state=ok,type=gauge ' \
-                                          f'value={rollingtotal} {x}\n'
+                                        f'value={count} {x}\n'
                             # r = requests.post(url, params=payload, data=postvalues)
-                        count = 1
-                        location += 1
-        else:
-            for line in content:
-                try:
-                    nextline = content[location]
-                except IndexError:
-                    nextline = content[location - 1]
-                if line['active'] == 'True' and line['start_date'] == nextline['start_date']:
-                    date1 = datetime.strptime(line['start_date'], "%Y-%m-%d")
-                    pairdate1 = int(calendar.timegm(date1.timetuple()) *
-                                    1000000000)
-                    count += 1
-                elif line['active'] == 'True' and line['start_date'] != nextline['start_date']:
-                    date2 = datetime.strptime(nextline['start_date'], "%Y-%m-%d")
-                    pairdate2 = int(calendar.timegm(date2.timetuple()) *
-                                    1000000000)
-                    rollingtotal += count
-                    if pairdate1 is None:
-                        date1 = datetime.strptime(line['start_date'], "%Y-%m-%d")
-                        pairdate1 = int(calendar.timegm(date1.timetuple()) *
-                                        1000000000)
-                    for x in range(pairdate1, pairdate2, options.step):
-                        postvalues += f'{options.timeseries},type_instance=num_provisioned_users,' \
-                                      f'ds_index=0,ds_name=value,' \
-                                      f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
-                                      f'plugin=statsd,state=ok,type=gauge ' \
-                                      f'value={rollingtotal} {x}\n'
-                        # r = requests.post(url, params=payload, data=postvalues)
-                    count = 1
-                location += 1
 
-    # check1
+                        count = 1
+                line1 = line2
+        postvalues = f'{options.timeseries},type_instance=num_provisioned_users,' \
+                     f'ds_index=0,ds_name=value,' \
+                     f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
+                     f'plugin=statsd,state=ok,type=gauge ' \
+                     f'value={count} {x + options.step}'
+        print(postvalues)
+        # r = requests.post(url, params=payload, data=postvalues)
     print("it worked!!!!!!!!!")
 
 if __name__ == "__main__":
