63de18c330569f524e56f590074ca4bd4f8acde6
Andres <alejarcialopez@gmail.com>
2017-08-08 09:57:52 +0100

    add system feedback and optional args

--- backfill.py
+++ backfill.py
@@ -32,8 +32,14 @@ def parse_post(dbfile, options):
                     if list1[0][0] == list2[0][0]:
                         count += 1
                     elif list1[0][0] != list2[0][0]:
-
-                        for x in range(pairdate1, pairdate2, options.step):
+                        if pairdate2 < pairdate1:
+                            b = pairdate1
+                            a = pairdate2
+                        else:
+                            a = pairdate1
+                            b = pairdate2
+                        print(a, b)
+                        for x in range(a, b, options.step):
                             postvalues += f'{options.timeseries},type_instance=num_provisioned_users,' \
                                           f'ds_index=0,ds_name=value,' \
                                           f'ds_type=gauge,host=selfservice-node4.srv.uis.private.cam.ac.uk,' \
@@ -50,15 +56,15 @@ def parse_post(dbfile, options):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="parse database of licenses and post rolling total and timestamps")
-    parser.add_argument("dbfile", type=str, help="file to be parsed")
-    parser.add_argument("timeseries", type=str, help="name of time series")
-    parser.add_argument("step", type=int, help="interval of how quickly measurements are taken")
+    parser.add_argument("-db", "--databasefile", dest="dbfile", type=str, help="file to be parsed")
+    parser.add_argument("-ts", "--timeseries", dest="timeseries", type=str, help="name of time series")
+    parser.add_argument("-i", "--interval", dest="step", type=int, help="interval of how quickly measurements are taken")
     parser.add_argument("-sd", "--stopdate", dest="stopd", type=str, default='4000-12-31', action="store",
                         help="store stop date (default: last date of file), date format: YYYY-MM-DD")
-    parser.add_argument("url", type=str, help="server url")
-    parser.add_argument("influx_db", type=str, help="database to write to")
-    parser.add_argument("influx_user", type=str, help="username")
-    parser.add_argument("influx_pass", type=str, help="password")
+    parser.add_argument("-l", "--link", dest="url", type=str, help="server url")
+    parser.add_argument("-sdb", "--serverdb", dest="influx_db", type=str, help="database to write to")
+    parser.add_argument("-su", "--serveruser", dest="influx_user", type=str, help="username")
+    parser.add_argument("-sp", "--serverpass", dest="influx_pass", type=str, help="password")
     args = parser.parse_args()
     parse_post(args.dbfile, args)
 
